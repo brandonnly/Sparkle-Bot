cf905f915fcbcc441e5733a4d0354ee102be8fea
brandonnly <bly852@gmail.com>
2020-10-31 17:17:21 -0400

    rewrote sparkle on and off

--- bot.py
+++ bot.py
@@ -1,49 +1,50 @@
+"""
+Main bot functions
+
+invite link: https://discord.com/oauth2/authorize?client_id=760945753379176498&scope=bot
+"""
+
 import discord
+from discord.ext import commands
 import re
 import os
 
 global sparkles
 sparkles = []
 
-client = discord.Client(everyone=False, here=False, roles=False)
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = discord.ext.commands.Bot('sparkle ', allowed_mentions=discord.AllowedMentions.none())
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    global sparkles
-    if message.author == client.user:
-        return
-    if message.content == "sparkle on":
-        sparkles.append(message.channel.id)
-        await message.add_reaction('👍')
-    elif message.content == "sparkle off":
-        sparkles.remove(message.channel.id)
-        await message.add_reaction('👍')
-    if sparkles and message.channel.id in sparkles and message.content != 'sparkle on':
+    if message.channel.id in sparkles and message.content.lower() != 'sparkle off':
+        # subs out spakles for sparkles and attaches sparkles to the front and back
         string = ":sparkles: " + re.sub('\s', ' :sparkles: ', message.content) + " :sparkles:"
-        string = re.sub('@', ' ', string)
         if len(string) > 2000:
             await message.channel.send("That message is too long by " + str(len(string) - 2000) + " characters!")
         await message.channel.send(string)
-    if message.content.lower() == 'ahlie':
+
+    if 'ahlie' in message.content.lower():
         await message.channel.send(":sparkles: TRUE :sparkles:")
-    if message.content.lower() == 'good morning':
+
+    if 'good morning' in message.content.lower():
         await message.channel.send(":sparkles: Good morning! :sparkles:")
-    if 'sparkle' in message.content and message.content != 'sparkle on' and not sparkles:
-        # make message content into string
-        string = message.content
-        # remove the initial sparkle
-        string = string.replace('sparkle ', '')
-        string = string.replace('sparkle', '')
-        # replace space characters with sparkles
-        string = re.sub('\s', ' :sparkles: ', string)
-        # prefix and append a sparkle
-        string = ":sparkles: " + string + " :sparkles:"
-        if len(string) > 2000:
-            await message.channel.send("That message is too long by " + str(len(string) - 2000) + " characters!")
-        await message.channel.send(string)
+
+    # allows the bot to process all the commands below
+    await bot.process_commands(message)
 
 
-client.run(BOT_TOKEN)
-# invite link: https://discord.com/oauth2/authorize?client_id=760945753379176498&scope=bot
+@bot.command()
+async def on(ctx):
+    sparkles.append(ctx.channel.id)
+    await ctx.message.add_reaction('👍')
+
+
+@bot.command()
+async def off(ctx):
+    sparkles.remove(ctx.channel.id)
+    await ctx.message.add_reaction('👍')
+
+
+bot.run(BOT_TOKEN)
