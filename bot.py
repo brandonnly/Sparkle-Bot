eff28b46eabaff3ccbd922bb1082fa6c30420c0f
brandonnly <bly852@gmail.com>
2020-10-31 18:43:41 -0400

    added roll reminder task

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ invite link: https://discord.com/oauth2/authorize?client_id=760945753379176498&s
 """
 
 import discord
-from discord.ext import commands
+from datetime import datetime
+from discord.ext import commands, tasks
 import re
 import os
-global sparkles
 
+global sparkles
+global mudae_reminder
 sparkles = []
+mudae_reminder = []
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = discord.ext.commands.Bot('sparkle ', allowed_mentions=discord.AllowedMentions.none())
@@ -31,24 +34,27 @@ async def on_message(message):
                 await message.channel.send(chunk)
         await message.channel.send(string)
 
+    # sparkles individual words
     elif 'sparkle' in message.content.lower():
         string = message.content.lower().split(' ')
         for x in range(len(string)):
-            if string[x+1] == 'message' or string[x+1] == 'on' or string[x+1] == 'off':
+            if string[x+1] == 'message' or string[x+1] == 'on' or string[x+1] == 'off' or string[x+1] == 'help' or string[x+1] == 'mudae':
                 break
             elif string[x] == 'sparkle':
                 string[x+1] = ":sparkles: " + string[x+1] + " :sparkles:"
                 string.pop(x)
                 break
-        if string[1] == 'message' or string[1] == 'on' or string[1] == 'off':
+        if string[1] == 'message' or string[1] == 'on' or string[1] == 'off' or string[1] == 'help' or string[1] == 'mudae':
             pass
         else:
             string = ' '.join(string)
             await message.channel.send(string)
 
+    # ahlie response
     elif 'ahlie' in message.content.lower():
         await message.channel.send(":sparkles: TRUE :sparkles:")
 
+    # good morning response
     elif 'good morning' in message.content.lower():
         await message.channel.send(":sparkles: Good morning! :sparkles:")
 
@@ -84,6 +90,29 @@ async def message(ctx, *, arg):
         for chunk in [string[i:i + 2000] for i in range(0, len(string), 2000)]:
             await ctx.send(chunk[21:len(chunk)])
     await ctx.send(string[37:len(string)])
+
+
+@bot.command()
+async def mudae(ctx):
+    """
+    Toggles mudae roll reminder in the current channel
+    """
+    if ctx.channel.id not in mudae_reminder:
+        mudae_reminder.append(ctx.channel.id)
+        await ctx.send("I'll send reminders to roll in this channel.")
+    else:
+        mudae_reminder.remove(ctx.channel.id)
+        await ctx.send("I'll stop sending reminders to roll in this channel.")
+
+
+@tasks.loop(seconds=55.0)
+async def roll_reminder():
+    if datetime.now().minute == 30:
+        for c in mudae_reminder:
+            channel = bot.get_channel(c)
+            await channel.send('✨ time ✨ to ✨ roll! ✨')
+
+roll_reminder.start()
 
 
 bot.run(BOT_TOKEN)
